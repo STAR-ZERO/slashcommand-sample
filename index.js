abd0b7eeac0874c0dcca9288b5b870ded0a1331b
STAR_ZERO <kenji.01.star@gmail.com>
2016-02-06 22:14:30 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var Botkit = require('botkit');
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+  debug: false,
+});
+
+var bot = controller.spawn({
+  token: process.env.token
+}).startRTM();
+
+// https://github.com/howdyai/botkit/issues/108
+bot.api.team.info({}, function(err, res) {
+  controller.storage.teams.save({
+    id: res.team.id
+  }, function(err) {
+  });
+});
+
+controller.setupWebserver(process.env.port, function(err, webserver) {
+  controller.createWebhookEndpoints(controller.webserver);
+});
+
+controller.on('slash_command', function(bot, message) {
+  if ('/hello' === message.command) {
+    bot.replyPublic(message, 'Hello!');
+  }
+});
